d7c26ee169809b8ff04ebdf92680ed58aa11e132
VladGrushevoi <vlad.grush.99@gmail.com>
2021-01-02 15:44:37 +0200

    some changing

--- src/Components/Parking.js
+++ src/Components/Parking.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom';
 
 function Parking(props) {
 
     const history = useHistory();
+    useEffect(()=>{
+        console.log(props)
+    });
     const setPark = (park) =>{
         history.push("/reservation")
         props.setPark(park.parkingId);
